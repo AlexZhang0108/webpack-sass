0c8ff068d0f7f396ebece401f1ac00c5fba0942d
AlexZhang0108 <17502192219@163.com>
2019-03-31 12:44:38 +0800

    webpack-sass

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+
+
+
+
+import css from "../css/index.scss"
+
+let Op = document.createElement('p')
+
+Op.innerHTML = 'Sass'
+
+document.body.appendChild(Op)
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+// cnpm i -D css-loader style-loader
+// cnpm i -D sass-loader node-sass
+// Cnpm i -D extract-text-webpack-plugin@next(插件-分离器)
+
+
+
+module.exports = {
+    mode: 'development',
+
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.js',
+    },
+
+    plugins: [
+        new htmlWebpackPlugin({
+            title: '第二天联系Sass'
+        }),
+        new ExtractTextPlugin('style.css')
+    ],
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+                use: [{
+                    loader: "css-loader"
+                }, {
+                    loader: "sass-loader"
+                }],
+                // 在开发环境使用 style-loader
+                fallback: "style-loader"
+            })
+        }]
+    }
+
+}
